1b0215eeebd2f678fd881cf800e8d6d323dbff46
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-09-24 17:52:11 +0000

    advanced task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,15 +1,19 @@
 # changing configuration file using puppet 
+file { 'config_file':
+  path    => '/etc/ssh/ssh_config',
+  ensure  => present,
+}
 
 file_line {'edit_password':
-  path    => '/etc/ssh/ssh_config'
-  line    => 'PasswordAuthentication no'
-  match   => '^#?PasswordAuthentication'
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  match   => '^#?PasswordAuthentication',
   ensure  => present
 }
 
 file_line {'edit_IdentityFile':
-  path    => '/etc/ssh/ssh_config'
-  line    => 'IdentityFile ~/.ssh/school'
-  match   => '^#?IdentityFile ~/.ssh/id_rsa'
-  ensure  => present
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/school',
+  match   => '^#?IdentityFile ~/.ssh/id_rsa',
+  ensure  => present,
 }
