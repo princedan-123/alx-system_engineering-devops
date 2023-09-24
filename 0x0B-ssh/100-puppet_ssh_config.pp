07699843045d38d0cbbf34a6de39aad36060b0d1
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-09-24 16:14:20 +0000

    advanced task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,9 @@
 # changing configuration file using puppet 
 
 augeas {'edit_config_file':
-  context => '/etc/ssh/ssh_config'
+  context => '/files/etc/ssh/ssh_config'
   changes => [
 'set PasswordAuthentication no',
-'set IdentityFile ~/.ssh/schooli',
+'set IdentityFile ~/.ssh/school',
 ],
 }
