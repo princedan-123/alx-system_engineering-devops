0d9a5e09bcd9b66c9f28fc23311f4ce65e7d1fa3
Daniel Ebuak Mabia <danielebukamabia@gmail.com>
2023-11-24 13:03:18 +0000

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# installing flask using puppet
+# installs flash version 2.1.0 using puppet
 
 exec {'flask':
-  command  => '/usr/bin/pip3 install Flask==2.1.0',
+  command => '/usr/bin/pip3 install flask==2.1.0',
 }
