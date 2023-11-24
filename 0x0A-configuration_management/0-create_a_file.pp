f91b2edd7d6a72ef05dedcea3d93c119addfcede
Daniel Ebuak Mabia <danielebukamabia@gmail.com>
2023-11-24 10:38:33 +0000

    task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#  creating a file using puppet
+
+file {'/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
