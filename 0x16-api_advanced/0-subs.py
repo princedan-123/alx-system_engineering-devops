2ff8b97daec5389cdf259323858597729df8f2c3
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-01-16 14:01:51 +0000

    added user-agent header

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,11 +6,14 @@ import requests
 
 def number_of_subscribers(subreddit):
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    response = requests.get(url, allow_redirects=False)
+    custom_header = {'User-Agent': 'myreddit_script'}
+    response = requests.get(url, allow_redirects=False, headers=custom_header)
     if response.status_code == 404:
         return 0
     elif response.status_code == 200:
         result = response.json()
         data = result.get('data')
         count = data.get('subscribers')
+        if count is None:
+            return 0
         return count
