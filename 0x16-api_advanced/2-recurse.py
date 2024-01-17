4adac81983a9fbb4e36ca61d844590a844817ad0
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-01-17 04:23:42 +0000

    ammended list appending

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,17 +9,19 @@ def recurse(subreddit, hot_list=[], after=None):
     header = {'User-Agent': '1-top_ten.py'}
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     response = requests.get(
-            url, params={'after': after}, headers=header, allow_redirects=False
+            url, params={'after': after, 'limit': 100},
+            headers=header, allow_redirects=False
             )
     if response.status_code == 404:
         return None
     else:
-        result = response.json().get('data', {}).get('children', [])
-        for data in result:
-            title = data.get('title')
+        result = response.json().get('data', {})
+        children = result.get('children', [])
+        for child in children:
+            title = child.get('data', {}).get('title', {})
             if title is not None:
                 hot_list.append(title)
-        after = response.json().get('data', {}).get('after')
+        after = result.get('after')
         if after is not None:
-            return recurse(subreddit, hot_list=hot_list, after=after)
+            return recurse(subreddit, hot_list, after=after)
         return hot_list
