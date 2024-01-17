9a46f3543970818ea406e1911b85812a51e64d35
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-01-17 12:26:06 +0000

    ammended path and removed sudo

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Changing the extension of a file using puppet
 
 exec {'wp-settings.php':
-  command => 'sed -i s/.phpp/.php/g var/www/html/wp-settings.php',
+  command => 'sed -i s/phpp/php/g var/www/html/wp-settings.php',
   path    => '/bin:/usr/bin:/usr/local/bin',
 }
