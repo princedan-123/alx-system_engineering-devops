1fdfb5881d877c22788ddee016aa69faa2f7510c
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-01-18 10:11:04 +0000

    added trailing / to path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Change all .phpp extension to .php
 
 exec {'wp-settings.php':
-  command => 'sed -i s/phpp/php/g var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/',
 }
